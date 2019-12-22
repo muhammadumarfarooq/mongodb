edfffc8c471a5a1450029ab903edf0e031cc83a0
Muhammad Umar Farooq <develop.umar@gmail.com>
2019-12-22 19:28:32 +0500

    user model

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectURI = "mongodb://umar:Get12345@ds357708.mlab.com:57708/demo-data";
 
-mongoose.connect(connectURI, { useNewUrlParser: true });
+mongoose.connect(connectURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 mongoose.connection
   .once("open", () => console.log("Good to go"))
   .on("error", error => {
